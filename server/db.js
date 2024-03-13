45a1e9396bb30bfa4391b6d4049533c9671e760d
Cris3h <cris.treachi@gmail.com>
2024-03-13 19:28:17 -0300

    version for deployment

--- server/db.js
+++ server/db.js
@@ -2,12 +2,21 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: 'todoapp'
+// })
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: 'todoapp'
+    connectionString: process.env.POSTGRES_URL ,
+  })
+
+pool.connect((err) => {
+    if(err) throw err
+    console.log("connected to PostgreSQL successfully")
 })
 
 module.exports = pool
