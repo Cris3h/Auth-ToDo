d5211ec8323bec9d8e3d69ebce9bcb8d28e3bc21
Cris3h <cris.treachi@gmail.com>
2024-03-13 19:49:58 -0300

    asd

--- server/db.js
+++ server/db.js
@@ -2,17 +2,14 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: 'todoapp'
-// })
-
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL ,
-  })
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: 'todoapp'
+})
+
 
 pool.connect((err) => {
     if(err) throw err
