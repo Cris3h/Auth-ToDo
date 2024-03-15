74b093bc14443cbbd5588fabcab2d6c26a291d73
Cris3h <cris.treachi@gmail.com>
2024-03-15 11:01:07 -0300

    deploy onrender

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const server = express();
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const database  = require('./schema');
+const database = require("./schema");
 
 const PORT = process.env.PORT ?? 8000;
 
@@ -16,8 +16,8 @@ const saltRounds = 10;
 server.get("/todos/:userEmail", async (req, res) => {
   const { userEmail } = req.params;
   try {
-    const todos = await database.ToDo.find({user_email: userEmail})
-    console.log(todos)
+    const todos = await database.ToDo.find({ user_email: userEmail });
+    console.log(todos);
     res.json(todos);
   } catch (error) {
     console.error({ er: error });
@@ -27,28 +27,30 @@ server.get("/todos/:userEmail", async (req, res) => {
 //create;
 server.post("/todos", async (req, res) => {
   const { user_email, title, progress, date } = req.body;
-  const id = uuidv4();
   try {
-    // const newToDo = await pool.query(
-    //   `INSERT INTO todos(id, user_email, title, progress, date) VALUES ($1, $2, $3, $4, $5)`,
-    //   [id, user_email, title, progress, date]
-    // );
-    // res.json(newToDo);
+    const newToDo = await database.ToDo.create({
+      user_email,
+      title,
+      progress,
+      date,
+    });
+    res.json(newToDo);
   } catch (err) {
     console.error("error", err);
   }
 });
 
 //edit
-server.put("/todos/:id", async (req, res) => {
+server.patch("/todos/:id", async (req, res) => {
   const { id } = req.params;
   const { user_email, title, progress, date } = req.body;
   try {
-    // const editToDo = await pool.query(
-    //   "UPDATE todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;",
-    //   [user_email, title, progress, date, id]
-    // );
-    // res.json(editToDo);
+    const editToDo = await database.ToDo.findByIdAndUpdate(
+      { _id: id },
+      { user_email, title, progress, date },
+      { new: true }
+    );
+    res.json(editToDo);
   } catch (err) {
     console.error(err);
   }
@@ -59,10 +61,8 @@ server.delete("/todos/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    // const deleteTodo = await pool.query("DELETE FROM todos WHERE id = $1;", [
-    //   id,
-    // ]);
-    // res.json(deleteTodo);
+    const deleteToDo = await database.ToDo.findOneAndDelete({ _id: id });
+    res.json(deleteToDo);
   } catch (error) {
     console.error(error);
   }
@@ -70,12 +70,16 @@ server.delete("/todos/:id", async (req, res) => {
 
 //signup
 server.post("/signup", async (req, res) => {
-  const { email, hashed_password } = req.body;
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hashedpassword = bcrypt.hashSync(hashed_password, salt);
+  const { email, password } = req.body;
 
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedpassword = bcrypt.hashSync(password, salt);
   try {
-    const signUp = await database.User.create({email, hashedpassword})
+    const signUp = new database.User({
+      email: email,
+      hashed_password: hashedpassword,
+    });
+    await signUp.save();
     const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
     res.json({ email, token });
   } catch (error) {
@@ -88,25 +92,16 @@ server.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // const users = await pool.query("SELECT * FROM users WHERE email = $1", [
-    //   email,
-    // ]);
-
-    // if (!users.rows.length) return res.json({ detail: "user doesn't exists!" });
-
-    // const success = await bcrypt.compare(
-    //   password,
-    //   users.rows[0].hashed_password
-    // );
-    // const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
-
-    // if (success) {
-    //   res.json({ email: users.rows[0].email, token });
-    // } else {
-    //   res.json({ detail: "login failed" });
-    // }
+    const user = await database.User.find({ email });
+    if (!user.length) return res.json({ detail: "user doesn't exists!" });
+    const success = await bcrypt.compare(password, user[0].hashed_password);
+    const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
+    if (success) {
+      res.json({ email: user[0].email, token });
+    } else {
+      res.json({ detail: "login failed" });
+    }
   } catch (error) {
-    // console.error(error);
     res.json({ errorDetail: error.detail });
   }
 });
