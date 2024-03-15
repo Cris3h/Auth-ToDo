260f17e3fc0f938e837f1493a61adb2bce963c93
Cris3h <cris.treachi@gmail.com>
2024-03-14 23:54:39 -0300

    deploy mongo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-const pool = require("./db");
 const express = require("express");
 const cors = require("cors");
 const server = express();
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const database  = require("./schema");
+const database  = require('./schema');
 
 const PORT = process.env.PORT ?? 8000;
 
@@ -19,7 +18,7 @@ server.get("/todos/:userEmail", async (req, res) => {
   try {
     const todos = await database.ToDo.find({user_email: userEmail})
     console.log(todos)
-    // res.json(todos);
+    res.json(todos);
   } catch (error) {
     console.error({ er: error });
   }
@@ -30,11 +29,11 @@ server.post("/todos", async (req, res) => {
   const { user_email, title, progress, date } = req.body;
   const id = uuidv4();
   try {
-    const newToDo = await pool.query(
-      `INSERT INTO todos(id, user_email, title, progress, date) VALUES ($1, $2, $3, $4, $5)`,
-      [id, user_email, title, progress, date]
-    );
-    res.json(newToDo);
+    // const newToDo = await pool.query(
+    //   `INSERT INTO todos(id, user_email, title, progress, date) VALUES ($1, $2, $3, $4, $5)`,
+    //   [id, user_email, title, progress, date]
+    // );
+    // res.json(newToDo);
   } catch (err) {
     console.error("error", err);
   }
@@ -45,11 +44,11 @@ server.put("/todos/:id", async (req, res) => {
   const { id } = req.params;
   const { user_email, title, progress, date } = req.body;
   try {
-    const editToDo = await pool.query(
-      "UPDATE todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;",
-      [user_email, title, progress, date, id]
-    );
-    res.json(editToDo);
+    // const editToDo = await pool.query(
+    //   "UPDATE todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;",
+    //   [user_email, title, progress, date, id]
+    // );
+    // res.json(editToDo);
   } catch (err) {
     console.error(err);
   }
@@ -60,10 +59,10 @@ server.delete("/todos/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    const deleteTodo = await pool.query("DELETE FROM todos WHERE id = $1;", [
-      id,
-    ]);
-    res.json(deleteTodo);
+    // const deleteTodo = await pool.query("DELETE FROM todos WHERE id = $1;", [
+    //   id,
+    // ]);
+    // res.json(deleteTodo);
   } catch (error) {
     console.error(error);
   }
@@ -76,8 +75,6 @@ server.post("/signup", async (req, res) => {
   const hashedpassword = bcrypt.hashSync(hashed_password, salt);
 
   try {
-    // const signUp = new User({email, hashedpassword})
-    // await signUp.save()
     const signUp = await database.User.create({email, hashedpassword})
     const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
     res.json({ email, token });
@@ -91,23 +88,23 @@ server.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const users = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
+    // const users = await pool.query("SELECT * FROM users WHERE email = $1", [
+    //   email,
+    // ]);
 
-    if (!users.rows.length) return res.json({ detail: "user doesn't exists!" });
+    // if (!users.rows.length) return res.json({ detail: "user doesn't exists!" });
 
-    const success = await bcrypt.compare(
-      password,
-      users.rows[0].hashed_password
-    );
-    const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
+    // const success = await bcrypt.compare(
+    //   password,
+    //   users.rows[0].hashed_password
+    // );
+    // const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
 
-    if (success) {
-      res.json({ email: users.rows[0].email, token });
-    } else {
-      res.json({ detail: "login failed" });
-    }
+    // if (success) {
+    //   res.json({ email: users.rows[0].email, token });
+    // } else {
+    //   res.json({ detail: "login failed" });
+    // }
   } catch (error) {
     // console.error(error);
     res.json({ errorDetail: error.detail });
