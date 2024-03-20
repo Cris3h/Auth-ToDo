b8accc19cdb97b38fc02236983401b3e3a5a9a80
Diiegote <diego.elprado22@gmail.com>
2024-03-19 23:05:34 -0300

    configurando cors

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const server = express();
 const morgan = require('morgan');
-
-const { v4: uuidv4 } = require("uuid");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const database = require("./schema");
 const router = require("./routes");
+const server = express();
 
 const PORT = process.env.PORT ?? 8000;
 
-server.use(cors());
+server.use(cors({
+  origin: 'https://auth-to-do-client.vercel.app',
+  credentials: true,
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'authorization',
+  ],
+}));
 server.use(express.json());
 server.use(morgan('dev'));
-const saltRounds = 10;
 
 
 server.use('/', router);
@@ -25,44 +29,5 @@ server.use((err, req, res, next) => {
     message: err.message
   });
 });
-
-
-// //signup
-// server.post("/signup", async (req, res) => {
-//   const { email, password } = req.body;
-
-//   const salt = bcrypt.genSaltSync(saltRounds);
-//   const hashedpassword = bcrypt.hashSync(password, salt);
-//   try {
-//     const signUp = new database.User({
-//       email: email,
-//       hashed_password: hashedpassword,
-//     });
-//     await signUp.save();
-//     const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
-//     res.json({ email, token });
-//   } catch (error) {
-//     if (error) res.json({ detail: error.detail });
-//   }
-// });
-
-// //login
-// server.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await database.User.find({ email });
-//     if (!user.length) return res.json({ detail: "user doesn't exists!" });
-//     const success = await bcrypt.compare(password, user[0].hashed_password);
-//     const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
-//     if (success) {
-//       res.json({ email: user[0].email, token });
-//     } else {
-//       res.json({ detail: "login failed" });
-//     }
-//   } catch (error) {
-//     res.json({ errorDetail: error.detail });
-//   }
-// });
 
 server.listen(PORT, () => console.log(`server running at port ${PORT}`));
